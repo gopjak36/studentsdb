b43fb38065da2871869d58dc6e133e29c8d59a14
gopjak36 <gop.jak.36@gmail.com>
2017-05-08 18:07:17 +0300

    add dynamics url and fix url error

--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -2,19 +2,17 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Student urls
+    # Students urls
     url(r'^$', 'students.views.students_list', name='home'),
     url(r'^students/add/$', 'students.views.students_add', name='students_add'),
-    url(r'^students/(?P<sid>\d+)/edit/$', 'students.views.students_edit', name='students_edit'),
-    url(r'^students/(?P<sid>\d+)/delete/$', 'students.views.students_delete', name='students_delete'),
+    url(r'^students/(?P<sid>\d+)/edit/$','students.views.students_edit', name='students_edit'),
+    url(r'^students/(?P<sid>\d+)/delete/$','students.views.students_delete', name='students_delete'),
 
-    #Groups urls
+    # Groups urls
     url(r'^groups/$', 'students.views.groups_list', name='groups'),
     url(r'^groups/add/$', 'students.views.groups_add', name='groups_add'),
-    url(r'^group/(?P<gid>\d+)/edit/$', 'students.views.groups_edit', name='groups_edit'),
-    url(r'^students/(?P<gid>\d+)/delete/$', '.students.views.groups_delete', name='groups_delete'),
+    url(r'^groups/(?P<gid>\d+)/edit/$','students.views.groups_edit', name='groups_edit'),
+    url(r'^groups/(?P<gid>\d+)/delete/$','students.views.groups_delete', name='groups_delete'),
 
-
-    #Admin
     url(r'^admin/', include(admin.site.urls)),
 )
