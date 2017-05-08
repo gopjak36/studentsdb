c4db38e49349fc6c2b30a5f917730ea592a72319
gopjak36 <gop.jak.36@gmail.com>
2017-05-08 15:15:45 +0300

    Revert "error url"
    This reverts commit 112e1dd213350358abb10843f5aeaa4325e50312.

--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -2,19 +2,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Student urls
-    url(r'^$', 'students.views.students_list', name='home'),
-    url(r'^students/add/$', 'students.views.students_add', name='students_add'),
-    url(r'^students/(?P<sid>\d+)/edit/$', 'students.views.students_edit', name='students_edit'),
-    url(r'^students/(?P<sid>\d+)/delete/$', 'students.views.students_delete', name='students_delete'),
+    # Examples:
+    # url(r'^$', 'studentsdb.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
 
-    #Groups urls
-    url(r'^groups/$', 'students.views.groups_list', name='groups'),
-    url(r'^groups/add/$', 'students.views.groups_add', name='groups_add'),
-    url(r'^group/(?P<gid>\d+)/edit/$', 'students.views.groups_edit', name='groups_edit'),
-    url(r'^students/(?P<gid>\d+)/delete/$', '.students.views.groups_delete', name='groups_delete'),
-
-
-    #Admin
     url(r'^admin/', include(admin.site.urls)),
 )
