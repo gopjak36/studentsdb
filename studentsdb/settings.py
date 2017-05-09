d21b215cfae84657483028ca62c742da764fbe10
gopjak36 <gop.jak.36@gmail.com>
2017-05-09 15:59:06 +0300

    add custom context processors

--- studentsdb/settings.py
+++ studentsdb/settings.py
@@ -88,4 +88,8 @@ from django.conf import global_settings
 
 TEMPLATE_CONTEXT_PROCESSORS = global_settings.TEMPLATE_CONTEXT_PROCESSORS + (
 "django.core.context_processors.request",
+"studentsdb.context_processors.students_proc",
 )
+
+# Add Portal URL
+PORTAL_URL ='http://localhost:8000'
