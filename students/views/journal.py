ad9ec9da0e366b03ddb91806a7effbc47de1da28
gopjak36 <gop.jak.36@gmail.com>
2017-05-12 15:44:32 +0300

    edit journal views and jornal page

--- students/views/journal.py
+++ students/views/journal.py
@@ -5,4 +5,12 @@ from django.http import HttpResponse
 # Views For Journal
 
 def journal_list(request):
-    return render(request, 'students/journal_list.html', {})
+    journal = (
+        {'id': 1,
+         'name': u'Подоба Віталій'},
+        {'id': 2,
+         'name': u'Корост Андрій'},
+        {'id': 3,
+         'name': u'Петрович Василь'},
+    )
+    return render(request, 'students/journal_list.html', {'journal': journal})
