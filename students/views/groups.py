a663b68fb6be4063c4e82bb036578dc977945430
gopjak36 <gop.jak.36@gmail.com>
2017-05-20 18:28:09 +0300

    add Order By for groups list

--- students/views/groups.py
+++ students/views/groups.py
@@ -8,6 +8,14 @@ from ..models import Group
 
 def groups_list(request):
     groups = Group.objects.all()
+
+    # Order By groups list
+    order_by = request.GET.get('order_by', '')
+    if order_by in ('title', 'leader'):
+        groups = groups.order_by(order_by)
+        if request.GET.get('reverse', '') == '1':
+            groups = groups.reverse()
+
     return render(request, 'students/groups_list.html',{'groups': groups})
 
 def groups_add(request):
