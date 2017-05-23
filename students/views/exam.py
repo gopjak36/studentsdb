ddee36a6356409dc78e150f550a6fd4e321643a6
gopjak36 <gop.jak.36@gmail.com>
2017-05-23 22:05:52 +0300

    addorder by for exams list

--- students/views/exam.py
+++ students/views/exam.py
@@ -9,4 +9,11 @@ from ..models import Exam
 def exams_list(request):
     exams = Exam.objects.all()
 
+    # Order By exams list
+    order_by = request.GET.get('order_by', '')
+    if order_by in ('name', 'groups', 'time'):
+        exams = exams.order_by(order_by)
+        if request.GET.get('reverse', '') == '1':
+            exams = exams.reverse()
+
     return render(request, 'students/exams_list.html', {'exams': exams})
