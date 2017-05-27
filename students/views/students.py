2dfb4f71fd9d47a925cae86b7e4e0f3b98ea275a
gopjak36 <gop.jak.36@gmail.com>
2017-05-27 20:59:22 +0300

    add template and update view for Student Form

--- students/views/students.py
+++ students/views/students.py
@@ -32,7 +32,7 @@ def students_list(request):
     return render(request, 'students/students_list.html', {'students': students})
 
 def students_add(request):
-    return HttpResponse('<h1>Students Add Form</h1>')
+    return render(request, 'students/students_add.html', {})
 
 def students_edit(request, sid):
     return HttpResponse('<h1>Edit Student %s</h1>' % sid)
