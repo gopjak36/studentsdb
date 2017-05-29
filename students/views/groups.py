e2af85412bfe116a9acd1946a6932a25744d91d1
gopjak36 <gop.jak.36@gmail.com>
2017-05-29 23:30:02 +0300

    add order by first column for groups list

--- students/views/groups.py
+++ students/views/groups.py
@@ -12,7 +12,7 @@ def groups_list(request):
 
     # Order By groups list
     order_by = request.GET.get('order_by', '')
-    if order_by in ('title', 'leader'):
+    if order_by in ('id', 'title', 'leader'):
         groups = groups.order_by(order_by)
         if request.GET.get('reverse', '') == '1':
             groups = groups.reverse()
