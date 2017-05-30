047cc364ceeebfc035d44c90cd1e6ae4ab15b268
gopjak36 <gop.jak.36@gmail.com>
2017-05-31 01:21:15 +0300

    update students add templates and view

--- students/views/students.py
+++ students/views/students.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-from ..models import Student
+from ..models import Student, Group
 
 #Views for Students
 
@@ -32,7 +32,7 @@ def students_list(request):
     return render(request, 'students/students_list.html', {'students': students})
 
 def students_add(request):
-    return render(request, 'students/students_add.html', {})
+    return render(request, 'students/students_add.html', {'groups': Group.objects.all().order_by('title')})
 
 def students_edit(request, sid):
     return HttpResponse('<h1>Edit Student %s</h1>' % sid)
