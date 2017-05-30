2666ea12ed05e65c883a34e9cfdaa642a75deae6
gopjak36 <gop.jak.36@gmail.com>
2017-05-30 23:05:06 +0300

    add order by for journal list

--- students/views/journal.py
+++ students/views/journal.py
@@ -8,4 +8,12 @@ from ..models import Journal
 
 def journal_list(request):
     journal = Journal.objects.all()
+
+    # Order By journal list
+    order_by = request.GET.get('order_by', '')
+    if order_by in ('id',):
+        journal = journal.order_by(order_by)
+        if request.GET.get('reverse', '') == '1':
+            journal = journal.reverse()
+
     return render(request, 'students/journal_list.html', {'journal': journal})
