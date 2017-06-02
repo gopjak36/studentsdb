4b75a03113ed03d019017ab090aaf7364ec9d4d2
gopjak36 <gop.jak.36@gmail.com>
2017-06-03 01:25:27 +0300

    update validate for student add, include: full validate for some input and save data after failed validation

--- students/views/students.py
+++ students/views/students.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from datetime import datetime
 
 from ..models import Student, Group
 
@@ -49,39 +50,48 @@ def students_add(request):
             # Validate user input
 
             # First name validate
-            first_name = request.POST.get('first_name').strip()
+            first_name = request.POST.get('first_name', '').strip()
             if not first_name:
                 errors['first_name'] = u"Ім’я є обов’язковим"
             else:
                 data['first_name'] = first_name
 
             # Last name validate
-            last_name = request.POST.get('last_name').strip()
+            last_name = request.POST.get('last_name', '').strip()
             if not last_name:
                 errors['last_name'] = u"Прізвище є обов’язковим"
             else:
                 data['last_name'] = last_name
 
             # Birthday validate
-            birthday = request.POST.get('birthday').strip()
+            birthday = request.POST.get('birthday', '').strip()
             if not birthday:
                 errors['birthday'] = u"Дата народження є обов’язковою"
             else:
-                data['birthday'] = birthday
+                try:
+                    datetime.strptime(birthday, '%Y-%m-%d')
+                except Exception:
+                    errors['birthday'] = u"Введіть коректний формат дати (напр. 1984-12-30)"
+                else:
+                    data['birthday'] = birthday
 
             # Ticket validate
-            ticket = request.POST.get('ticket')
+            ticket = request.POST.get('ticket', '').strip()
             if not ticket:
                 errors['ticket'] = u"Номер білета є обов’язковим"
             else:
-                data['ticket'] = ticket
+                    data['ticket'] = ticket
 
             # Student Group validate
-            student_group = request.POST.get('student_group')
+            student_group = request.POST.get('student_group','').strip()
             if not student_group:
                 errors['student_group'] = u"Оберіть групу для студента"
             else:
-                data['student_group'] = student_group
+                groups = Group.objects.filter(pk=student_group)
+                if len(groups) !=1:
+                    errors['student_group'] = u"Оберіть коректну групу"
+                else:
+                    data['student_group'] = student_group
 
             # Photo validate
             photo = request.FILES.get('photo')
@@ -102,7 +112,7 @@ def students_add(request):
             else:
                 # render form with errors and previus user input
                 return render(request, 'students/students_add.html',
-                       {'group': Group.objects.all().order_by('title'),
+                       {'groups': Group.objects.all().order_by('title'),
                         'errors': errors })
 
         # Form cancel burron clicked?
