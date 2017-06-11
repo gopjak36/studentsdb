fe3c2383e59e9fe3a1e6cc51a74f5ba29bc73a5c
gopjak36 <gop.jak.36@gmail.com>
2017-06-11 17:01:55 +0300

    customize admin panel for Students list

--- students/admin.py
+++ students/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
 from .models import Student, Group, Journal, Exam, Result
+from django.core.urlresolvers import reverse
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['last_name', 'first_name', 'ticket', 'student_group']
+    list_display_links = ['last_name', 'first_name']
+    list_editable = ['student_group']
+    ordering = ['last_name']
+    list_filter = ['student_group']
+    list_per_page = 10
+    search_fields = ['last_name', 'first_name', 'middle_name', 'ticket', 'notes']
+
+    def view_on_site(self,obj):
+        return reverse('students_edit', kwargs={'pk': obj.id})
 
 # Register Student model:
-admin.site.register(Student)
+admin.site.register(Student, StudentAdmin)
 admin.site.register(Group)
 admin.site.register(Journal)
 admin.site.register(Exam)
