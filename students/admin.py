aa23f7bc6a183398165687ff210efd6534d401f0
gopjak36 <gop.jak.36@gmail.com>
2017-06-12 15:29:43 +0300

    customize admin panel for Groups list

--- students/admin.py
+++ students/admin.py
@@ -19,8 +19,6 @@ class StudentFormAdmin(ModelForm):
         return self.cleaned_data['student_group']
 
 class StudentAdmin(admin.ModelAdmin):
-
-    form = StudentFormAdmin
     list_display = ['last_name', 'first_name', 'ticket', 'student_group']
     list_display_links = ['last_name', 'first_name']
     list_editable = ['student_group']
@@ -29,14 +27,25 @@ class StudentAdmin(admin.ModelAdmin):
     list_per_page = 10
     search_fields = ['last_name', 'first_name', 'middle_name', 'ticket', 'notes']
 
-
+    form = StudentFormAdmin
 
     def view_on_site(self,obj):
         return reverse('students_edit', kwargs={'pk': obj.id})
 
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ['title', 'leader']
+    list_display_link = ['title']
+    ordering = ['title']
+    list_filter = ['title']
+    list_per_page = 10
+    search_fields = ['title', 'notes']
+
+    def view_on_site(self, obj):
+        return reverse('groups_edit', kwargs={'gid': obj.id})
+
 # Register Student model:
 admin.site.register(Student, StudentAdmin)
-admin.site.register(Group)
+admin.site.register(Group, GroupAdmin)
 admin.site.register(Journal)
 admin.site.register(Exam)
 admin.site.register(Result)
