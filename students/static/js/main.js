1f420d885fd2d1a7395a2dff6e9acf40a7747545
gopjak36 <gop.jak.36@gmail.com>
2017-07-03 12:33:13 +0300

    update Modal function for Student Editm (view form blank with data)

--- students/static/js/main.js
+++ students/static/js/main.js
@@ -57,10 +57,35 @@ function initDateFields() {
   });
 }
 
-function initEditStudentpage() {
+function initEditStudentPage() {
   $('a.student-edit-form-link').click(function(event){
-    var modal = $('#myModal');
-    modal.modal('show');
+    var link = $(this);
+    $.ajax({
+      'url': link.attr('href'),
+      'dataType': 'html',
+      'type': 'get',
+      'success': function(data, status, xhr){
+        // check if we got successfull response from the server:
+        if (status != 'success'){
+          alert('Помилка на сервері. Спробуйте будь-ласка пізніше.');
+          return false;
+        }
+
+        // update modal window with arrived content from server:
+        var modal = $('#myModal'),
+          html = $(data), form = html.find('#content-column form');
+        modal.find('.modal-title').html(html.find('#content-column h2').text());
+        modal.find('.modal-body').html(form);
+
+        // setup ans show modal window:
+        modal.modal('show');
+      },
+      'error': function(){
+        alert('Помилка на сервері. Спробуйте будь-ласка пізніше.');
+        return false;
+      }
+    });
+
     return false;
   });
 }
@@ -69,5 +94,5 @@ $(document).ready(function(){
   initJournal();
   initGroupSelector();
   initDateFields();
-  initEditStudentpage();
+  initEditStudentPage();
 });
