b433d8c5cbf931ecc6f6e36a3857e4700ed8e1dd
gopjak36 <gop.jak.36@gmail.com>
2017-07-13 18:39:54 +0300

    update view, save data to database and redirect pages, for Groups add

--- students/views/groups.py
+++ students/views/groups.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from ..models import Group, Student
@@ -32,7 +33,36 @@ def groups_list(request):
     return render(request, 'students/groups_list.html',{'groups': groups})
 
 def groups_add(request):
-    return render(request, 'students/groups_add.html', {'students': Student.objects.all().order_by('last_name')})
+
+    # Form POST == YES:
+    if request.method == 'POST':
+        # add_button == push:
+        if request.POST.get('add_button') is not None:
+            # TODO: Validate input from user:
+            errors ={}
+
+            if not errors:
+                # create new group object:
+                group = Group(
+                title=request.POST['title'],
+                leader=Student.objects.get(pk=request.POST['leader']),
+                notes=request.POST['notes'],
+                )
+                # save group to database:
+                group.save()
+                # redirect to group list:
+                return HttpResponseRedirect(reverse('groups'))
+            else:
+                # redirect form with erros and previus user input:
+                return render(request, 'students/groups_add.html', {'groups':Group.objects.all().order_by('last_name'),'errors': errors})
+        # cancel_button == push:
+        elif request.POST.get('cancel_button') is not None:
+            # redirect to group_list:
+            return HttpResponseRedirect(reverse('groups'))
+    # Form POST == NO:
+    else:
+        # initial form render:
+        return render(request, 'students/groups_add.html', {'students': Student.objects.all().order_by('last_name')})
 
 def groups_edit(request, gid):
     return HttpResponse('<h1>Edit Group %s</h1>' % gid)
