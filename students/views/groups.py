a26d90f9ffcb8bf64a65cea87947a03cd446ebdd
gopjak36 <gop.jak.36@gmail.com>
2017-07-17 14:45:59 +0300

    create class GroupForm(style with Crispy Forms) for Add and Edit Group, fix bag with leader in Group Edit

--- students/views/groups.py
+++ students/views/groups.py
@@ -5,6 +5,10 @@ from django.core.urlresolvers import reverse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from django.views.generic.edit import CreateView
+from django.forms import ModelForm
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit
+
 from ..models import Group, Student
 
 # Views For Groups
@@ -33,9 +37,37 @@ def groups_list(request):
 
     return render(request, 'students/groups_list.html',{'groups': groups})
 
+class GroupForm(ModelForm):
+
+    class Meta:
+        model = Group
+
+    def __init__(self,*args,**kwargs):
+        super(GroupForm, self).__init__(*args,**kwargs)
+        self.helper = FormHelper(self)
+        # Tag atribute for Form:
+        self.helper.form_action = reverse('groups_add') # Add Group Form
+        self.helper.form_method = 'POST'
+        self.helper.form_class = 'form-horizontal'
+        # Fields propertiies for Form:
+        self.helper.help_text_inline = True
+        self.helper.label_class = 'col-sm-2 control-label'
+        self.helper.field_class = 'col-sm-10'
+        # Stucture of Form:
+        self.helper.layout = Layout(
+            'title',
+            'leader',
+            'notes',
+            # Button for Form:
+            Submit('add_button', u'Зберегти', css_class='btn btn-primary'),
+            Submit('cancel_button', u'Скасувати', css_class='btn btn-danger'),
+        )
+
+
 class GroupCreateView(CreateView):
     model = Group # Model for add Group
     template_name = 'students/groups_add.html' # Template for form
+    form_class = GroupForm # style of form
 
     # Add_button == PUSH and not ValidationErrors:
     def get_success_url(self):
@@ -50,8 +82,6 @@ class GroupCreateView(CreateView):
         else:
             # initial Form render:
             return super(GroupCreateView, self).post(request,*args,**kwargs)
-
-
 
 def groups_edit(request, gid):
     ''' Edit groups method '''
